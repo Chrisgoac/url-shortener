6584e59f3e477376d0f83a7966c7031043317f39
Christian González <chrisgonzaco@gmail.com>
2024-06-05 22:04:06 +0200

    added vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import node from "@astrojs/node";
 import auth from "auth-astro";
 import db from "@astrojs/db";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   integrations: [auth(), db(), react(), tailwind()]
 });
